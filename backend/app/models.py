12df02afc6aa42678125cd8ff4a8940c5e0129b5
Andrea Fan <andrea.fan@ringus-solution.com>
2024-12-03 13:18:39 +0800

    Update: backend

---
+++ backend/app/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+class Event(Base):
+    __tablename__ = 'events'
+
+    id = Column(Integer, primary_key=True, index=True)
+    event_type = Column(String, index=True)
+    page_url= Column(String)
+    user_agent = Column(String, nullable=True)
+    ip_address = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
