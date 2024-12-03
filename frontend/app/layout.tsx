865d6b70ac2fa7a12202ee52632abefefd24a23c
Andrea Fan <andrea.fan@ringus-solution.com>
2024-12-03 16:18:51 +0800

    Update

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Unlimited Dashboard",
+  description: "Unlimited Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>
+        <SidebarProvider>
+          <SidebarTrigger />
+          <AppSidebar />
+          <main>{children}</main>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
---
+++ frontend/components/app-sidebar.tsx
@@ -0,0 +1,55 @@
+import { Eye, Ghost } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarGroup,
+  SidebarFooter,
+  SidebarGroupLabel,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from "@/components/ui/sidebar";
+
+const sidebarItems = [
+  {
+    title: "Overview",
+    url: "/dashboard",
+    icon: Eye,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-2xl font-bold mb-4">
+            <a href="/" className="flex items-center">
+              <Ghost className="mr-2" />
+              Dashboard
+            </a>
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {sidebarItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
